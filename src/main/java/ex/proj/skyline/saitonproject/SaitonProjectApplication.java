01e6d1f4d49a3feafc8e438c451c87aa9c5f5aee
Vlad-Buko <vladislav_buko@mail.ru>
2023-02-07 23:53:40 +0300

    Start commit

---
+++ src/main/java/ex/proj/skyline/saitonproject/SaitonProjectApplication.java
@@ -0,0 +1,11 @@
+package ex.proj.skyline.saitonproject;
+
+
+@SpringBootApplication
+public class SaitonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaitonProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ex/proj/skyline/saitonproject/SaitonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ex.proj.skyline.saitonproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaitonProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
