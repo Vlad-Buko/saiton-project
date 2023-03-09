9a715a921e70564711f96682c3a89e5e2a433e51
Vlad-Buko <vladislav_buko@mail.ru>
2023-03-10 01:19:04 +0300

    Added security config, not full

---
+++ src/main/java/ex/proj/skyline/saitonproject/dto/Person.java
@@ -0,0 +1,38 @@
+package ex.proj.skyline.saitonproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by Vladislav Domaniewski
+ */
+
+@Entity
+@Table(name = "person")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "Имя не должно быть пустым")
+    @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов длиной")
+    private String username;
+
+    @Min(value = 1900, message = "Год рождения должен быть больше, чем 1900")
+    @Column(name = "year_of_birth")
+    private int yearOfBirth;
+
+    private String password;
+}
