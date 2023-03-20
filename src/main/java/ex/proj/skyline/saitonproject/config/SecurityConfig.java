5abfd72ee816f05b3e1573f8ebb44c2ca768b76d
Vlad-Buko <vladislav_buko@mail.ru>
2023-03-20 23:02:11 +0300

    Was be change basic logic , specifically to make realization spring security

--- src/main/java/ex/proj/skyline/saitonproject/config/SecurityConfig.java
+++ src/main/java/ex/proj/skyline/saitonproject/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package ex.proj.skyline.saitonproject.config;
 
-import ex.proj.skyline.saitonproject.config.security.AuthProviderImpl;
+import ex.proj.skyline.saitonproject.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Created by Vladislav Domaniewski
@@ -12,17 +15,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final AuthProviderImpl authProvider;
+
+    private final PersonService personService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonService personService) {
+        this.personService = personService;
     }
 
 
-
     // Настройка аутентификации
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider); //
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personService); //
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEcod() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
