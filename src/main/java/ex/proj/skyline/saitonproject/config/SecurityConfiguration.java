89962d20b83261ff5887364de07858e8f311db3f
Vlad-Buko <vladislav_buko@mail.ru>
2023-04-19 23:50:58 +0300

    Added dependency for gitHub authorization

--- src/main/java/ex/proj/skyline/saitonproject/config/SecurityConfiguration.java
+++ src/main/java/ex/proj/skyline/saitonproject/config/SecurityConfiguration.java
@@ -5,9 +5,7 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,31 +15,31 @@ import static org.springframework.security.config.Customizer.withDefaults;
 /**
  * Created by Vladislav Domaniewski
  */
-
-@Configuration
-@Log4j2
-public class SecurityConfiguration {
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .formLogin(withDefaults())
-                .oauth2Login(withDefaults())
-                .authorizeRequests(c -> c.anyRequest().authenticated())
-                .build();
-    }
-
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-
-    @Bean
-    ApplicationListener<AuthenticationSuccessEvent> successLogger() {
-        return event -> {
-            log.info("success: {}", event.getAuthentication());
-        };
-    }
-}
+//
+//@Configuration
+//@Log4j2
+//public class SecurityConfiguration {
+//
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .formLogin(withDefaults())
+//                .oauth2Login(withDefaults())
+//                .authorizeRequests(c -> c.anyRequest().authenticated())
+//                .build();
+//    }
+//
+//
+//    @Bean
+//    PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//
+//    @Bean
+//    ApplicationListener<AuthenticationSuccessEvent> successLogger() {
+//        return event -> {
+//            log.info("success: {}", event.getAuthentication());
+//        };
+//    }
+//}
